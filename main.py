a2c206bd7c0fd59c61a001ff47fdd76da048263c
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-10-27 13:46:53 +0300

    pivot to langchain + langgraph

--- main.py
+++ main.py
@@ -1,139 +1,107 @@
-import  os, asyncio, json, dotenv, zipfile
-from pathlib import Path
-from crewai.flow.flow import Flow, listen, start
-from crewai_tools import PDFSearchTool
-from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-#craw4ai
-from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, BrowserConfig, LLMConfig
-from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
-from crawl4ai.content_filter_strategy import PruningContentFilter
+from typing import List, Literal, TypedDict, Annotated
+from langchain_core.messages.tool import tool_call
+from langgraph.graph import StateGraph,START, END
+from langchain.messages import AnyMessage, SystemMessage, ToolMessage, HumanMessage
+from langchain.chat_models import init_chat_model
+from langchain_deepseek import ChatDeepSeek
+from langchain.tools import tool
+import operator
 
-from core.crew import crew
-
-class StudyMetaData(BaseModel):
-    country: str = ""
-
-class SigState(BaseModel):
-    study_url: str = ""
-    study_path: str = ""
-    study_title: str = " "
-    study_meta: StudyMetaData = None
+#graph state
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls : int
 
 
+# Initialize DeepSeek model
+model = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0,
+)
 
-class sigFlow(Flow[SigState]):
-    @start()
-    def ask(self):
-        self.state.study_url = "https://bmcmicrobiol.biomedcentral.com/articles/10.1186/s12866-025-04242-7"
-        return self.state
+@tool
+def multiply(x: int, y: int) -> int:
+    """Multiply two numbers together."""
+    return x * y
 
-    @listen(ask)
-    async def scrape(self, state):
-        
-        # downloads path
-        knowledge_path = os.path.join(os.getcwd(), "knowledge")
-        os.makedirs(knowledge_path, exist_ok=True)
+@tool
+def add(x: int, y: int) -> int:
+    """Add two numbers together."""
+    return x + y
 
-        # # main input area
-        # core = os.path.join(os.getcwd(), "core")
-        # os.makedirs(core, exist_ok=True)
-
-        browser_config = BrowserConfig(
-            # downloadable files
-            accept_downloads=True,
-            downloads_path=knowledge_path,
-        )
-
-        config = CrawlerRunConfig(
-            exclude_social_media_links=True,
-            capture_mhtml=True,  # capture mhtml
-            # exclude_external_links=True, #exclude external links from the result
-            markdown_generator=DefaultMarkdownGenerator(
-                content_filter=PruningContentFilter(
-                    threshold_type="dynamic", threshold=0.5
-                ),
-                options={
-                    "ignore_images": True,
-                    "skip_internal_links": True,  # skip internal links inside the markdwon but allow it to be scraped.
-                    "ignore_links": True,
-                },
-            ),
-            # javascript code to trigger file download
-            js_code="""
-                // Try to find and click supplementary material download links
-                const suppLinks = document.querySelectorAll('a[href*="/s1"], a[href*="download"], .supplementary-material a');
-                suppLinks.forEach(link => {
-                    if (link.href && !link.href.includes('#')) {
-                        link.click();
-                        console.log('Clickedl supplementary material link:', link.href);
-                    }
-                });
-            """,
-            wait_for="5",  # wait for 5 seconds !important: this has to be a string not an integer
-        )
-
-        async with AsyncWebCrawler(config=browser_config) as crawler:
-            result = await crawler.arun(url=state.study_url, config=config)
-
-            if result.downloaded_files:
-                for file_path in result.downloaded_files:
-                    if file_path.endswith(".zip"):
-                        with zipfile.ZipFile(file_path, "r") as zip_ref:
-                            zip_ref.extractall(knowledge_path)
-
-            if result:
-                # markdown
-                md_file = "knowledge/study.md"
-                with open(md_file, "w", encoding="utf-8") as f:
-                    f.write(result.markdown)
-                    state.study_path = md_file
-
-                # mhtml
-                if result.mhtml:
-                    mhtml_file = "advanced.mhtml"
-                    with open(mhtml_file, "w", encoding="utf-8") as f:
-                        f.write(result.mhtml)
-
-                # media
-                if result.media:
-                    with open("knowledge/images.json", "w") as f:
-                        json.dump(result.media.get("images", []), f)
-
-        return self.state
+@tool
+def divide(x: int, y: int) -> float:
+    """Divide one number by another."""
+    if y == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return x / y
     
-    @listen(scrape)
-    def converter(self):
-        know = Path('knowledge')
-        study = Path('knowledge/study.md')
-        
-        for file in know.iterdir():
-            if file.suffix == '.json':
-                with open(file, 'r') as f:
-                    data = json.load(f)
-                    with open(study, 'a') as md:
-                        md.write('## Image Description Data' + '\n\n')
-                        for item in data:
-                            img_desc = item['desc']
-                            md.write(img_desc + '\n\n')
-                print(file.name)
+
+
+
+#tools config
+tools = [multiply, divide, add]
+tools_by_name = {tool.name: tool for tool in tools}
+llm_with_tools = model.bind_tools(tools)
+
+
+
+#model node
+def llm_call(state: AgentState) -> AgentState:
+    """Agent that computes numbers using the tools provided and operation passed add as story to the output / a fun fact about africa"""
+    return {
+        'messages': [
+            llm_with_tools.invoke(
+                [
+                    SystemMessage(content="You are a helpful assistant that can perform basic math operations and provide interesting facts about Africa."),  
                 
-            else:
-                pass
+                ] + state['messages']
+            )
             
-        
-        return self.state
+        ],
+    }
     
-    
-    @listen(converter)
-    def crew_call(self):
-        output = os.path.join(os.getcwd(), 'output/')
-        os.makedirs(output, exist_ok=True)
-        
-        inputs = {'study_path': self.state.study_path}
-        crew.kickoff(inputs=inputs)
-        return self.state
+
+
+
+#tool Node
+def tool_node(state: AgentState):  
+    """Performs the tool call based on the tool name and arguments provided"""
+    result = []
+    print(state['messages'][-1])
+    for tool_call in state['messages'][-1].tool_calls:
+        tool = tools_by_name[tool_call['name']]
+        observation = tool.invoke(tool_call)
+        print(tool_call)
+        result.append(ToolMessage(content=observation, tool_call_id = tool_call['id']))
+        return {'messages': result}
         
 
-if __name__ == "__main__":
-    sigFlow().kickoff()
+
+
+#Conditional logic
+def should_continue(state: AgentState) -> Literal['tool_node', END]:
+    """Decides whether to continue with tool_node or end the conversation"""
+    messages = state['messages']
+    last_message = messages[-1]
+    if last_message.tool_calls:
+        return 'tool_node'
+    return END
+    
+
+
+graph = StateGraph(AgentState)
+graph.add_node('llm_call', llm_call).add_node('tool_node', tool_node)
+graph.add_edge(START, 'llm_call').add_conditional_edges('llm_call', should_continue, {'tool_node', END}).add_edge('tool_node', 'llm_call')
+
+agent = graph.compile()
+messages = [HumanMessage(content="What is 2 + 2?")]
+messages = agent.invoke({"messages": messages})
+
+for m in messages['messages']:
+    print(m.content)
+    print("\n")
