f8c971e7ebe2641930c6e89801a0d3ecfd0fce60
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-11-24 12:13:41 +0300

    refining the embedding model and using langchain community approach instead of chroma db approach

--- nodes/embed.py
+++ nodes/embed.py
@@ -2,7 +2,7 @@ from pprint import pprint
 import chromadb , uuid
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from lib.models import ollama_ef
+from langchain_ollama import OllamaEmbeddings
 
 
 
@@ -11,44 +11,26 @@ def embbed(state):
     collection = client.get_or_create_collection(name="documents")
 
     doc_path = Path("core/study.md").read_text()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2024,
-        chunk_overlap=256,
-    )
-
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_text(doc_path)
 
-
-    def embbedder(chunk):
-        embedd = ollama_ef([chunk])
-        return embedd[0][0]
+    #generate embeddings
+    embeddingsModel = OllamaEmbeddings(model="nomic-embed-text:latest")
 
 
     print("============  Generating embeddings  ============ \n")
 
-    for chunk in chunks:
-        # Generate embedding for the chunk
+    # Generate embedding for the chunk
+    embeddings = embeddingsModel.embed_documents(chunks)    
 
-        # Add document with its embedding to the collection
-        collection.add(
-            ids=[str(uuid.uuid4())],
-            documents=[chunk],
-            # embeddings=[embedding]
-        )
+    # Add embeddings to the chroma db
+    collection.add(ids = [str(uuid.uuid4()) for _ in range(len(chunks))], documents=chunks)
 
-
-    results=collection.query(
-        query_texts = [
-            "what is sonder",
-            'what does sonder represent'
-        ],
-        n_results = 5
-    )
-
-
-    for i, query_res in enumerate(results['documents']): 
-        print(i)
-        pprint(query_res)
-        print('\n')
+    # results=collection.query(
+    #     query_texts = [
+    #         "how many experiments were done",
+    #     ],
+    #     n_results = 3
+    # )
     
     return state
